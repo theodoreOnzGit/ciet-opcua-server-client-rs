4ebfef0b45e9d5cf3285f7b805d7e6bc245d309e
teddy0-mint-box <theodore_ong@berkeley.edu>
2023-10-19 15:41:17 +0800

    server tested working on linux mint

--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -3,9 +3,10 @@ pub use ciet_libraries::*;
 
 
 pub mod examples;
-pub use examples::ciet_server_old_with_deviation::*;
+pub use examples::ciet_server_old_with_deviation;
 
 fn main() {
-    println!("Hello, from server!");
+    let run_server = true;
+    ciet_server_old_with_deviation::construct_and_run_ciet_server(run_server);
 }
 
