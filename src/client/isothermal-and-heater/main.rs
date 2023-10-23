d988f705cd5c85f98784ae0f3f45fd31c4ddc962
teddy0-mint-box <theodore_ong@berkeley.edu>
2023-10-23 13:44:16 +0800

    crit damp decay modes okay

--- src/client/isothermal-and-heater/main.rs
+++ src/client/isothermal-and-heater/main.rs
@@ -59,7 +59,16 @@ fn main() -> eframe::Result<()> {
     let mut g_s_second_order_underdamped = SecondOrderStableTransferFn::new(
         1.0, // process gain
         Time::new::<second>(1.0),  // process time
-        0.15, // damping factor
+        0.45, // damping factor
+        0.0, 
+        0.0, 
+        Time::new::<second>(1.0)
+    );
+
+    let mut g_s_second_order_crit_damped = SecondOrderStableTransferFn::new(
+        1.0, // process gain
+        Time::new::<second>(1.0),  // process time
+        0.5, // damping factor
         0.0, 
         0.0, 
         Time::new::<second>(1.0)
@@ -90,10 +99,10 @@ fn main() -> eframe::Result<()> {
 
             let current_time = Time::new::<millisecond>(time_elapsed_ms as f64);
 
-            let model_output = g_s_second_order_underdamped.set_user_input_and_calc_output(
+            let model_output = g_s_second_order_crit_damped.set_user_input_and_calc_output(
                 current_time, user_input as f64);
 
-            dbg!(&g_s_second_order_underdamped);
+            dbg!(&g_s_second_order_crit_damped);
 
             input_output_plots_ptr_clone.lock().unwrap().deref_mut()
                 .push([time_elapsed_s,user_input as f64,
